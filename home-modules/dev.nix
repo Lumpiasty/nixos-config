cb139e654da3822c6f70e57a7d841470cb3b0699
Lumpiasty <arek.dzski@gmail.com>
2025-08-25 01:29:32 +0200

    update flake and remove vscode version pin

--- home-modules/dev.nix
+++ home-modules/dev.nix
@@ -13,14 +13,6 @@
 
     programs.vscode = {
       enable = true;
-      package = pkgs.vscode.overrideAttrs rec {
-        version = "1.103.0";
-        src = pkgs.fetchurl {
-          name = "VSCode_${version}_linux-x64.tar.gz";
-          url = "https://update.code.visualstudio.com/${version}/linux-x64/stable";
-          hash = "sha256-Fji3/9T8X2VQH6gUhReSuniuX2BX+4S7uPJWEZn56vc=";
-        };
-      };
       profiles.default.extensions = [
         pkgs.vscode-extensions.github.copilot
         pkgs.vscode-extensions.github.copilot-chat
