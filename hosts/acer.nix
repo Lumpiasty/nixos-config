9e28e3eba6f957a3cc53bebc5720a4b8e1f7a50e
Lumpiasty <arek.dzski@gmail.com>
2025-10-02 14:57:07 +0200

    enable corectrl and amdgpu overdrive on acer

--- hosts/acer.nix
+++ hosts/acer.nix
@@ -116,6 +116,10 @@ rec {
 
   boot.kernelParams = [ "pcie_aspm=off" ];
 
+
+  programs.corectrl.enable = true;
+  hardware.amdgpu.overdrive.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
