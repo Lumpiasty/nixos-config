d7009f8ebbe0bc91b281e661180d745367a00c12
Lumpiasty <arek.dzski@gmail.com>
2025-10-02 14:56:11 +0200

    disable aspm on acer

--- hosts/acer.nix
+++ hosts/acer.nix
@@ -114,6 +114,8 @@ rec {
 
   boot.initrd.kernelModules = [ "amdgpu" "thunderbolt" ];
 
+  boot.kernelParams = [ "pcie_aspm=off" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
