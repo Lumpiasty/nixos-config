cb59a2e331ed9230e45b68566c4dc3247fc09cb3
Lumpiasty <arek.dzski@gmail.com>
2026-04-23 23:45:11 +0200

    clean up commented out version pinned kwin

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,16 +1,6 @@
 self: super:
 {
   opencode-claude-auth = super.callPackage ../pkgs/opencode-claude-auth {};
-  # Pin some kde packages to 6.3.1, 6.3.2 breaks thunderbolt
-  # kdePackages = super.kdePackages.overrideScope(kdeFinal: kdePrev: {
-  #   kwin = kdePrev.kwin.overrideAttrs (prevAttrs: {
-  #     src = super.fetchurl {
-  #       url = "mirror://kde/stable/plasma/6.3.1/kwin-6.3.1.tar.xz";
-  #       hash = "sha256-mlC6DqpiCXg73vu2aOV9DL36cc6Ov70X/kRtttdz8kI=";
-  #     };
-  #     version = "6.3.1";
-  #   });
-  # });
   linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
     acer-wmi-ext = lpsuper.callPackage ../pkgs/acer-wmi-ext {};
   });
