47169bbd0cebfa7c910ef2b3337b70b91148bc2d
Lumpiasty <arek.dzski@gmail.com>
2026-05-08 00:37:44 +0200

    workaround for openldap failure

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,4 +1,9 @@
 self: super:
 {
   opencode-claude-auth = super.callPackage ../pkgs/opencode-claude-auth {};
+  # Build failure 08.05.2026
+  # https://github.com/NixOS/nixpkgs/issues/513245#issuecomment-4320293674
+  openldap = super.openldap.overrideAttrs {
+    doCheck = !super.stdenv.hostPlatform.isi686;
+  };
 }
