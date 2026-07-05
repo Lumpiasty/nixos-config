2acb83570555f99f0c976171d2e9fc92748b9154
Lumpiasty <arek.dzski@gmail.com>
2026-07-05 15:04:45 +0200

    configure peerix

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -27,6 +27,24 @@ in
 
   services.peerix = {
     enable = true;
-    trackerUrl = null;  # Use LAN mode instead of Iroh
+    
+    # Use an empty string (not null) to use the modern unified app 
+    # but with the internet tracker internally disabled.
+    trackerUrl = "''"; 
+    
+    # Explicitly enable UDP broadcast discovery for your local network
+    lanDiscovery = true; 
+    
+    # Disable the upstream cache.nixos.org checks so you can share ALL packages
+    noFilter = true;
+    noVerify = true; 
+    
+    # Configure signing so Nix doesn't reject your custom unsigned packages
+    privateKeyFile = "/etc/cache-priv-key.pem";
+    
+    # Add the public key contents from `cache-pub-key.pem`
+    # (If you used different keys on each machine, you can pass a list of strings
+    # directly to nix.settings.extra-trusted-public-keys instead)
+    publicKey = "my-lan-cache:SVN4NaqHJKxdAbWZ6wUtklmKkBUrACPg8oihH/mlp4Y="; 
   };
 }
